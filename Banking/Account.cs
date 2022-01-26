90de94ea37b040af92356abbe3bc5ac50847d5c4
Ryan Stewart <Ryanps914@yahoo.com>
2022-01-26 11:57:03 -0500

    Add project files.

---
+++ Banking/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking {
+
+    public abstract class Account {
+
+        public int Id { get; set; }
+        public decimal Balance { get; set; } = 0;
+        public string Desription { get; set; }
+
+        private void CheckAmount(decimal Amount) {
+            if (Amount <= 0) {
+                throw new ArgumentException("Amount cannot be negative or zero");
+            }
+        }
+        public void Transfer(decimal Amount, Account ToAccount) {
+            this.Withdraw(Amount);
+            ToAccount.Deposit(Amount);
+        }
+        public void Deposit(decimal Amount) {
+            CheckAmount(Amount);
+            Balance += Amount;
+        }
+        public void Withdraw(decimal Amount) {
+            CheckAmount(Amount);
+            if (Amount > Balance) {
+                throw new Exception("Insuffecient funds");
+
+            }
+
+              
+            
+            Balance -= Amount;
+            }
+
+       // public Account(int Id) {
+           // this.Id = Id;
+       // }
+
+        public override string ToString() {
+            return $"{Id} |  {Balance} | {Desription}";
+        }
+    }
+}
+
---
+++ Banking/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sav1 = new Savings() {
+                Id = 200, Desription = "1st Savings"
+            };
+            sav1.Deposit(100);
+            sav1.CalculateAndPayInterest(1);
+            Console.WriteLine(sav1);
+            
+
+           var acct1 = new Account() { 
+            Id = 100, Balance = 100, Desription = "1st Account"
+              };
+              var acct2 = new Account() {
+              Id = 200,
+              Balance = 0,
+               Desription = "2nd Account"
+            };
+            acct1.Deposit(100);
+            Console.WriteLine(acct1);
+            acct1.Withdraw(20);
+            Console.WriteLine(acct1);
+            // acct1.Deposit(-40);
+            // Console.WriteLine(acct1);
+            //acct1.Deposit(-50);
+            //Console.WriteLine(acct1);
+            acct1.Transfer(30, acct2);
+        }
+    }
+}
